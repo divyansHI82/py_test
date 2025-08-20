33d627558a9572d909537cd6d0538be56dfe214e
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-08-20 21:49:34 +0530

    Add files via upload
    Write a program in python to demonstrate Keywords.

---
+++ Keywords.py
@@ -0,0 +1,12 @@
+variable=6
+if variable>5:
+    print("Variable is greater than 5")
+else:
+    print("Variable is 5 or less")
+    count=0
+    while count<5:
+        print(count)
+        count+=1
+        for i in range(5):
+            print(i)
+            print("This program is written by Divyanshi Singhal ERP I'D is 0221BCA150")
