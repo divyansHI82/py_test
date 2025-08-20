68893a8d4a4496623cbd4c20ddf9deb9d127948f
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-08-20 22:31:47 +0530

    Add files via upload
    Write a program in python to demonstrate Indentation.

---
+++ Indentation.py
@@ -0,0 +1,13 @@
+def my_function():
+    
+    print("This is properly idented")
+
+   
+    if True:
+        print("This will cause an error")
+        def my_other_function():  
+            print("This will cause an error")
+    
+    for i in range(5):
+        my_function()
+        print("This program is written by Divyanshi Singhal ERP I'D is 0221BCA150")
