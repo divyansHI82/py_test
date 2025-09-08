6c569a58fac00290f0282891ec94b107eefce0ab
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-08 22:54:45 +0530

    Delete year From 1-2025.py

--- year From 1-2025.py
+++
@@ -1,8 +0,0 @@
-count = 0
-for year in range(1, 2026):
-    if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
-        print(year, end=" ")
-        count += 1
-
-print("\nTotal leap years:", count)
-print("This program is written by Divyanshi Singhal ERP I.D is 0221BCH150")
