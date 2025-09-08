af50a12c9d8b3902ead5fe091eb8e32f2cc92471
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-08 22:40:16 +0530

    Update and rename Separate And Spaces Commas.py to 35_Separate And Spaces Commas.py

---
+++ 35_Separate And Spaces Commas.py
@@ -0,0 +1,3 @@
+numbers = list(map(int, input().split(',')))
+print(numbers)
+print("This program is written by Divyanshi Singhal ERP I.D is 0221BCH150")
--- Separate And Spaces Commas.py
+++
@@ -1,3 +0,0 @@
-numbers = list(map(int, input().split(',')))
-print(numbers)
-print("This program is written by Divyanshi Singhal ERP I.D is 0221BCH150")
