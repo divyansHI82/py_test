f43f1f2763ea171ab66444ebe8c38315e31f4bb0
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-22 21:57:37 +0530

    Create 69_GlobalVariables.py

---
+++ 69_GlobalVariables.py
@@ -0,0 +1,11 @@
+# Write a python code to demonstrate global variables.
+
+global_var = "I am Global"
+
+def example_function():
+    print(global_var)
+    
+example_function()
+print(global_var)
+
+print("\nThis program is written and executed by Divyanshi Singhal (0221BCA150)\n")
