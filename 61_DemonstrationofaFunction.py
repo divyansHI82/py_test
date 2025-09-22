12bf452eab97e91d4c08a78b5e1fe7e376fac8be
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-22 21:46:32 +0530

    Create 61_DemonstrationofaFunction.py

---
+++ 61_DemonstrationofaFunction.py
@@ -0,0 +1,8 @@
+# Write a python code to demonstrate function.
+
+def greet(name):
+    print(f"Hello, {name}")
+    
+greet("Alice")
+
+print("\nThis program is written and executed by Divyanshi Singhal (0221BCA150)\n")
