ce53389770b537ef4d3345f195cfe3d839be49c3
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-22 21:47:32 +0530

    Create 62_DemonstrationofFuncReturn.py

---
+++ 62_DemonstrationofFuncReturn.py
@@ -0,0 +1,10 @@
+# Write a python code to demonstrate Function with Return.
+
+def add(a,b):
+    return a+b
+
+result = add(3,5)
+
+print(result)
+
+print("\nThis program is written and executed by Divyanshi Singhal (0221BCA150)\n")
