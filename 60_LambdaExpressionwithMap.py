4bd18904b0788f44c96c524488b77b85c3e2c83c
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-22 21:44:38 +0530

    Create 60_LambdaExpressionwithMap.py

---
+++ 60_LambdaExpressionwithMap.py
@@ -0,0 +1,9 @@
+# Write a python code to use lambda Expressions with map().
+
+numbers = [1,2,3,4]
+
+squared = list(map(lambda x:x**2, numbers))
+
+print(squared)
+
+print("\nThis program is written and executed by Divyanshi Singhal (0221BCA150)\n")
