59d95d67f30190c166155b23f54ddd69cb97580c
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-22 21:50:19 +0530

    Create 64_DemonstrateFuncwithMultipleReturn.py

---
+++ 64_DemonstrateFuncwithMultipleReturn.py
@@ -0,0 +1,13 @@
+# Write a python code to demonstrate function with Multiple Returns.
+
+def get_user_info():
+    name = "Alice"
+    age = 30
+    return name, age
+
+user_name, user_age = get_user_info()
+
+print(user_name)
+print(user_age)
+
+print("\nThis program is written and executed by Divyanshi Singhal (0221BCA150)\n")
