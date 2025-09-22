4f3d0a8672c9d09b9e696647713b2aa38adb58f1
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-22 21:48:44 +0530

    Create 63_DemonstrateFuncwithDefaultParameters.py

---
+++ 63_DemonstrateFuncwithDefaultParameters.py
@@ -0,0 +1,9 @@
+# Write a python code to demonstrate Function with Default Parameters.
+
+def greet(name="Guest"):
+    print(f"Hello,{name}!")
+    
+greet()
+greet("Alice")
+
+print("\nThis program is written and executed by Divyanshi Singhal (0221BCA150)\n")
