37b0084640b4d8322cc360ca349c633c84208f50
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-09-22 21:43:14 +0530

    Create 59_SetComprehension.py

---
+++ 59_SetComprehension.py
@@ -0,0 +1,7 @@
+# Write a python code to demonstrate set comprehension.
+
+squares = {x**2 for x in range (5)}
+
+print(squares)
+
+print("\nThis program is written and executed by Divyanshi Singhal (0221BCA150)\n")
