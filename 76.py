e7106d05686f4eafad97f2cde9e03717110f1dd5
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-10-28 20:52:13 +0530

    Create 76.py

---
+++ 76.py
@@ -0,0 +1,17 @@
+# Python program to demonstrate docstring in a function
+
+def add(a, b):
+    """This function returns the sum of two numbers."""
+    return a + b
+
+# Calling the function
+result = add(5, 3)
+
+# Printing result
+print("Sum:", result)
+
+# Displaying the docstring of the function
+print("Function Docstring:", add.__doc__)
+
+
+print("\nThis program is written by Divyanshi Singhal ERP id is 0221BCA150.")
