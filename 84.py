70ff8f541409672aab0af26c5fdf8c2dffe5f845
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-10-28 21:04:31 +0530

    Create 84.py

---
+++ 84.py
@@ -0,0 +1,9 @@
+# Python program to import a specific function from a module
+
+from math import sqrt, factorial  # Importing only required functions
+
+num = 36
+print("Square root of", num, "is:", sqrt(num))
+print("Factorial of 6 is:", factorial(6))
+
+print("\nThis program is written by Divyanshi Singhal ERP id is 0221BCA150.")
