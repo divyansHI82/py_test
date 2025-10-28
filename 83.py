121fa5daabe32f22294d963e77de59fbe4fadd81
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-10-28 21:03:26 +0530

    Add Python program using math module
    This program demonstrates basic math operations using the math module.

---
+++ 83.py
@@ -0,0 +1,11 @@
+# Python program to demonstrate using a module
+
+import math  # Importing math module
+
+num = 25
+
+print("Square root of", num, "is:", math.sqrt(num))
+print("Value of pi is:", math.pi)
+print("Factorial of 5 is:", math.factorial(5))
+
+print("\nThis program is written by Divyanshi Singhal ERP id is 0221BCA150.")
