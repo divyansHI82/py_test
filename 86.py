27b35185a096ad36b23ab3a9be2d830978e2e9f3
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-10-28 21:09:41 +0530

    Add demonstration script for standard library modules
    This script demonstrates the use of several standard library modules in Python, including math, random, datetime, and os.

---
+++ 86.py
@@ -0,0 +1,27 @@
+# Demonstrating some standard library modules in Python
+
+import math        # For mathematical functions
+import random      # For generating random numbers
+import datetime    # For working with dates and times
+import os          # For operating system related functions
+
+print("=== Demonstration of Standard Library Modules ===\n")
+
+# math module example
+print("math Module:")
+print("Square root of 16:", math.sqrt(16))
+print("Value of π:", math.pi, "\n")
+
+# random module example
+print("random Module:")
+print("Random number between 1 and 10:", random.randint(1, 10), "\n")
+
+# datetime module example
+print("datetime Module:")
+print("Current date and time:", datetime.datetime.now(), "\n")
+
+# os module example
+print("os Module:")
+print("Current Working Directory:", os.getcwd(), "\n")
+
+print("\nThis program is written by Divyanshi Singhal ERP id is 0221BCA150.")
