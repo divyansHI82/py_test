5c09442ba1ee6a9f58a7e5505b5a3fa648858ff2
Divyanshi Singhal <divyanshisinghal555@gmail.com>
2025-10-28 21:07:53 +0530

    Create 85.py

---
+++ 85.py
@@ -0,0 +1,9 @@
+# Importing all functions from a module (example: math module)
+from math import *
+
+# Using a few functions from the math module
+print(sqrt(25))      # Square root of 25
+print(factorial(5))  # Factorial of 5
+print(pow(2, 3))     # 2 raised to the power 3
+
+print("\nThis program is written by Divyanshi Singhal ERP id is 0221BCA150.")
